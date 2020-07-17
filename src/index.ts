9d36c2b5f9270495b3dba674728310f115441af5
dfireBird <aadhishrm11@gmail.com>
2020-07-17 17:23:50 +0530

    📦 NEW: Add mongoose init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 import Bot from "./bot";
+import mongoose from "mongoose";
 import "dotenv/config";
 
-new Bot(process.env.BOT_TOKEN || "");
+const username = process.env.MONGO_INITDB_ROOT_USERNAME;
+const password = process.env.MONGO_INITDB_ROOT_PASSWORD;
+
+mongoose
+    .connect(`mongodb://${username}:${password}@db:27017/`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("✅ Connected to database");
+        new Bot(process.env.BOT_TOKEN || "");
+    })
+    .catch((err) => console.log(err));
