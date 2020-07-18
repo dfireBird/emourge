7983b8c9daabf11db157bfc717374f92bf9735ed
dfireBird <aadhishrm11@gmail.com>
2020-07-18 17:31:16 +0530

    🐛 FIX: Forgot to add interface definition

--- src/emojiModel.ts
+++ src/emojiModel.ts
@@ -1,26 +1,31 @@
 import mongoose, { Schema, Document } from "mongoose";
 
-export interface IEmojiFrequency extends Document { }
+export interface IEmojiFrequency {
+    emojiId: String;
+    emojiName: String;
+    frequency: Number;
+}
+
+export interface IEmojiFrequencyDb extends Document {
+    id: string;
+    emojiFrequency: IEmojiFrequency;
+}
 
 const emojiFrequencySchema = new Schema({
     id: {
         type: String,
         required: true,
     },
-    guildName: {
-        type: String,
-        required: true,
-    },
     emojiFrequency: [
         {
-            emojiId: Number,
+            emojiId: String,
             emojiName: String,
             frequency: Number,
         },
     ],
 });
 
-export const emojiFrequencyModel = mongoose.model<IEmojiFrequency>(
+export const emojiFrequencyModel = mongoose.model<IEmojiFrequencyDb>(
     "emojiFrequency",
     emojiFrequencySchema
 );
