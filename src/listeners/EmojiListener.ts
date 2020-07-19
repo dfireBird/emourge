8e52433fda204e9b843bc0b08f8aa79ae498ba01
dfireBird <aadhishrm11@gmail.com>
2020-07-19 21:36:35 +0530

    📦 NEW: Add listener to add freuency to db whenever an emoji is used

---
+++ src/listeners/EmojiListener.ts
@@ -0,0 +1,58 @@
+import { Listener } from "discord-akairo";
+import { Message } from "discord.js";
+import { extractEmoji } from "../utils";
+import { guildModel } from "../models/guildModel";
+
+export default class EmojiListener extends Listener {
+    constructor() {
+        super("message", {
+            emitter: "client",
+            event: "message",
+        });
+    }
+
+    public exec(msg: Message) {
+        this.storeEmoji(msg);
+    }
+
+    /*
+     * Seprates emojis from the message string and store it in database
+     *
+     * @param msg: Message object
+     */
+    private async storeEmoji(msg: Message) {
+        const emojis = extractEmoji(msg.content);
+        const emojiIds = emojis.map((emoji) => {
+            const result = emoji.match(/\d+/);
+            return result !== null ? result[0] : "";
+        });
+
+        if (msg.guild === null) return;
+
+        const emojiManager = msg.guild.emojis;
+        for (const emojiId of emojiIds) {
+            let emoji = emojiManager.resolve(emojiId);
+            if (emoji === null) return;
+
+            if (emoji.guild === msg.guild) {
+                const guild = await guildModel
+                    .findOne({ id: msg.guild.id })
+                    .exec();
+
+                if (guild === null) return;
+
+                const index = guild.emojiFrequency.findIndex((e) => {
+                    if (emoji === null) return;
+                    return e.emojiId == emoji.id;
+                });
+
+                guild.emojiFrequency[index].frequency++;
+                try {
+                    await guild.save();
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+        }
+    }
+}
