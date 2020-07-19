67a728e14b2b0bb3cae9b0056846b5b5dc70021d
dfireBird <aadhishrm11@gmail.com>
2020-07-19 21:34:18 +0530

    📦 NEW: Add init command to init the database for the guild

---
+++ src/commands/init.ts
@@ -0,0 +1,36 @@
+import { Command } from "discord-akairo";
+import { Message } from "discord.js";
+import { IEmojiFrequency, guildModel } from "../models/guildModel";
+
+class InitCommand extends Command {
+    constructor() {
+        super("init", {
+            aliases: ["init", "start"],
+        });
+    }
+
+    public exec(msg: Message) {
+        if (!msg.guild) {
+            return;
+        }
+        const emojis = msg.guild.emojis.cache;
+        let guildEmojis: IEmojiFrequency[] = [];
+        for (const [_key, emoji] of emojis) {
+            guildEmojis.push({
+                emojiId: emoji.id,
+                emojiName: emoji.name,
+                frequency: 0,
+            });
+        }
+        const guild = new guildModel({
+            id: msg.guild.id,
+            emojiFrequency: guildEmojis,
+        });
+
+        guild.save().then(() => {
+            msg.reply("Successfully Initialized");
+        });
+    }
+}
+
+export default InitCommand;
