3721c8ca1afe1c29d65da5830e4bcf4e0d40f506
dfireBird <aadhishrm11@gmail.com>
2020-07-21 12:55:26 +0530

    📦 NEW: Add help command

---
+++ src/commands/help.ts
@@ -0,0 +1,33 @@
+import { Command } from "discord-akairo";
+import { Message, MessageEmbed } from "discord.js";
+
+class HelpCommand extends Command {
+    constructor() {
+        super("help", {
+            aliases: ["help", "h"],
+            channel: "guild",
+        });
+    }
+
+    public exec(msg: Message) {
+        const embed = new MessageEmbed()
+            .setColor("#86F97C")
+            .setTitle("Help Information")
+            .setDescription("List of all commands")
+            .addFields(
+                { name: "`init`", value: "Starts recording emoji usage" },
+                {
+                    name: "`show-animated`",
+                    value: "Show least used animated emojis",
+                },
+                {
+                    name: "`show-standard`",
+                    value: "Show least used standard emojis",
+                }
+            );
+
+        return msg.channel.send(embed);
+    }
+}
+
+export default HelpCommand;
