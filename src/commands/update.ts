144914a57109442348f9cb33f1c3ffb010f044fc
dfireBird <aadhishrm11@gmail.com>
2020-07-22 14:28:55 +0530

    📦 NEW: Add update command

---
+++ src/commands/update.ts
@@ -0,0 +1,44 @@
+import { Command } from "discord-akairo";
+import { Message } from "discord.js";
+import { guildModel, IEmojiFrequency } from "../models/guildModel";
+
+class UpdateCommand extends Command {
+    constructor() {
+        super("update", {
+            aliases: ["update"],
+            channel: "guild",
+        });
+    }
+
+    public async exec(msg: Message) {
+        if (msg.guild === null) return;
+        const emojis = msg.guild.emojis.cache;
+        const guild = await guildModel.findOne({ id: msg.guild.id }).exec();
+        if (guild === null) return;
+
+        const updatedEmojis: IEmojiFrequency[] = emojis.map((emoji) => {
+            const found = guild.emojiFrequency.find(
+                (dbEmoji) => dbEmoji.emojiId === emoji.id
+            );
+            if (found) {
+                return found;
+            } else {
+                return {
+                    emojiId: emoji.id,
+                    emojiName: emoji.name,
+                    frequency: 0,
+                };
+            }
+        });
+
+        guild.emojiFrequency = updatedEmojis;
+        try {
+            await guild.save();
+        } catch (err) {
+            console.log(err);
+        }
+        return msg.channel.send("Information update successful");
+    }
+}
+
+export default UpdateCommand;
