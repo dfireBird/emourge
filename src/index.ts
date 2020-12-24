7021f0150349e54f1b3fb707f8086a5dd09540ce
dfireBird <me@dfirebird.codes>
2020-12-24 09:35:06 +0530

    chore: migrate from mongoose to typeorm connection (untested)

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,9 @@
 import Bot from "./bot";
-import mongoose from "mongoose";
+import { createConnection } from "typeorm";
+import "reflect-metadata";
 import "dotenv/config";
 
-const username = process.env.MONGO_INITDB_ROOT_USERNAME;
-const password = process.env.MONGO_INITDB_ROOT_PASSWORD;
-const host = process.env.MONGO_HOST ? process.env.MONGO_HOST : "db";
-
-mongoose
-    .connect(
-        `mongodb://${username}:${password}@${host}:27017/${process.env.MONGO_INITDB_DATABASE}`,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }
-    )
+createConnection()
     .then(() => {
         console.log("✅ Connected to database");
         new Bot(process.env.BOT_TOKEN || "");
